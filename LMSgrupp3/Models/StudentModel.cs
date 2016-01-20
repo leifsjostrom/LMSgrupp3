fc2974ff71ab3d657f7424a379ed7ee05e649834
leifsjostrom <lsjostro2010@telia.com>
2016-01-20 09:49:32 +0100

    Aded StudentModel.cs

---
+++ LMSgrupp3/Models/StudentModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMSgrupp3.Models
+{
+    public class StudentModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Adress { get; set; }
+
+        [Required]
+        public string Town { get; set; }
+
+        [Required]
+        public string ZipCode { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+}
