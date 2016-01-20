c1dec7a507fbf9fb1f3814b4cd58fd37869167be
leifsjostrom <lsjostro2010@telia.com>
2016-01-20 11:11:02 +0100

    Added FileModel.cs

---
+++ LMSgrupp3/Models/FileModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMSgrupp3.Models
+{
+    public class FileModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public TeacherModel Teacher { get; set; }
+
+        [Required]
+        public CourceModel Cource { get; set; }
+
+        [Required]
+        public bool Shared { get; set; }
+
+    }
+}
