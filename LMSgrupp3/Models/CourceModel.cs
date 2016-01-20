12331f6c61fdf952485a0fd7ad447264ebc2c91f
leifsjostrom <lsjostro2010@telia.com>
2016-01-20 10:53:43 +0100

    Added CourceModel.cs

---
+++ LMSgrupp3/Models/CourceModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMSgrupp3.Models
+{
+    public class CourceModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public TeacherModel Teacher { get; set; }
+
+        [Required]
+        public List<StudentModel> Students { get; set; }
+
+    }
+}
