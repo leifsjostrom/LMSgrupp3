0593c5d950359e3cad97843fc1d69370e5aca8f0
leifsjostrom <lsjostro2010@telia.com>
2016-01-20 11:01:40 +0100

    Added SchemaModel.cs

---
+++ LMSgrupp3/Models/SchemaModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMSgrupp3.Models
+{
+    public class SchemaModel
+    {
+        [Required]
+        public TeacherModel Teacher { get; set; }
+
+        [Required]
+        public CourceModel Cource { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public DateTime  StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+    }
+}
