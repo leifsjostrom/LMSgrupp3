9117ee19c731819024884da54349fccd21197d32
leifsjostrom <lsjostro2010@telia.com>
2016-02-01 10:08:24 +0100

    Updated routeconfig

--- LMSgrupp3/App_Start/RouteConfig.cs
+++ LMSgrupp3/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LMSgrupp3
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "SchemaModels", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
