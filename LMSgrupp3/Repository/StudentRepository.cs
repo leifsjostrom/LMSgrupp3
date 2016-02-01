a600c019c8b200177c54e4e3923e484ced8fdd98
leifsjostrom <lsjostro2010@telia.com>
2016-02-01 11:18:26 +0100

    Updated student repo

--- LMSgrupp3/Repository/StudentRepository.cs
+++ LMSgrupp3/Repository/StudentRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
 using LMSgrupp3.DataConnection;
 using LMSgrupp3.Models;
@@ -15,10 +16,34 @@ namespace LMSgrupp3.Repository
 
         public List<StudentModel> ShowAll()
         //Returns a list of all students
-        //{
-        //    var query = db.Students
-        //        .Include(m => m.StudentNumber);
-        //    return query.ToList();
-        //}
+        {
+            var query = db.Students;
+               // .Include(m => m.StudentNumber);
+            return query.ToList();
+        }
+
+
+
+        public bool Create(StudentModel student)
+        // Method to create student in db, takes studentModel as argument
+        {
+            //List<String> regs = db.Students.Select(sm => sm.reg).ToList();
+            //if (!regs.Contains(student.StudentNumber))
+
+            //vehicle.ID = db.Vehicles.Count() + 1;
+            try {
+            db.Students.Add(student);
+            db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                string innerex = ex.InnerException.Message;
+                string msg = ex.Message;
+            }
+            return true;
+            
+        }
+
+
     }
 }
