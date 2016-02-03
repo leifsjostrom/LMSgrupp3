0a451a391bfedf1ea2d569adb6abe372508f2803
leifsjostrom <lsjostro2010@telia.com>
2016-02-03 09:35:33 +0100

    After new schafolding

--- LMSgrupp3/Migrations/Configuration.cs
+++ LMSgrupp3/Migrations/Configuration.cs
@@ -4,6 +4,8 @@ namespace LMSgrupp3.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using LMSgrupp3.DataConnection;
+    using LMSgrupp3.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<LMSgrupp3.DataConnection.StudentContext>
     {
@@ -14,18 +16,88 @@ namespace LMSgrupp3.Migrations
 
         protected override void Seed(LMSgrupp3.DataConnection.StudentContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Teachers.AddOrUpdate(
+                new TeacherModel
+                {
+                    EmplymentNumber = "1001",
+                    Name = "Arwid Nordquist",
+                    Adress = "Guldgatan 45",
+                    Town = "Umea",
+                    ZipCode = "901 56",
+                    Email = "AnNord@yahoo.com"
+                },
+                new TeacherModel
+                {
+                    EmplymentNumber = "1002",
+                    Name = "Molly Melon",
+                    Adress = "Snårstigen 11",
+                    Town = "Umea",
+                    ZipCode = "901 50",
+                    Email = "MoMello@yahoo.com"
+                }
+              );
+            context.SaveChanges();
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Cources.AddOrUpdate(
+                new CourceModel
+                {
+                    Id = 1,
+                    Name = "Datavetenskap grundkurs",
+                    TeacherId = "1001",
+                    Students = null
+                }
+                );
+            context.SaveChanges();
+
+            context.Files.AddOrUpdate(
+                new FileModel
+                {
+                    Id = 1,
+                    Name = "Inlämingsuppgift 1",
+                    Subject = "Problembeskrivning",
+                    Shared = true,
+                    CourceId = 1,
+                    Date = DateTime.Now
+                }
+            );
+            context.SaveChanges();
+
+
+            context.Schemas.AddOrUpdate(
+                new SchemaModel
+                {
+                    CourceId = 1,
+                    Id = 101,
+                    TeacherId = "1001",
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now,
+                    Location = "Aula"
+                }
+            );
+
+            context.Students.AddOrUpdate
+                (
+                new StudentModel
+                {
+                    StudentNumber = "1001",
+                    Name = "Arne Arg",
+                    Email = "",
+                    Adress = "",
+                    Town = "",
+                    ZipCode = "",
+                    FileId = 0
+                },
+                new StudentModel
+                {
+                    StudentNumber = "1002",
+                    Name = "Bea Borg",
+                    Email = "",
+                    Adress = "",
+                    Town = "",
+                    ZipCode = "",
+                    FileId = 1
+                }
+                );
         }
     }
 }
