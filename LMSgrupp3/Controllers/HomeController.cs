b26106a75443dd52740ab5540bb3adf11d3f1ebf
leifsjostrom <lsjostro2010@telia.com>
2016-02-09 11:03:38 +0100

    Added file upload and download

--- LMSgrupp3/Controllers/HomeController.cs
+++ LMSgrupp3/Controllers/HomeController.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace LMSgrupp3.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+               LMSgrupp3.Views.FileTransfer ft = new LMSgrupp3.Views.FileTransfer();
+               ft.Upload(file);
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Download(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                LMSgrupp3.Views.FileTransfer ft = new LMSgrupp3.Views.FileTransfer();
+                ft.Download(file);
+            }
             return View();
         }
 
@@ -23,7 +47,10 @@ namespace LMSgrupp3.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            /*
+            LMSgrupp3.Views.FileTransfer ft = new LMSgrupp3.Views.FileTransfer();
+            ft.Download("minfil.txt");
+            */
             return View();
         }
     }
---
+++ LMSgrupp3/Views/FileTransfer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace LMSgrupp3.Views
+{
+    public class FileTransfer
+    {
+        public void Download(HttpPostedFileBase file)
+        {
+            try
+            {
+                // set the http content type to "APPLICATION/OCTET-STREAM
+                System.Web.HttpContext.Current.Response.ContentType = "APPLICATION/OCTET-STREAM";
+
+                // initialize the http content-disposition header to
+                // indicate a file attachment with the default filename
+                // "myFile.txt"
+                string disHeader = "Attachment; Filename=\"" + file.FileName +  "\"";
+                System.Web.HttpContext.Current.Response.AppendHeader("Content-Disposition", disHeader);
+
+                // transfer the file byte-by-byte to the response object
+                System.IO.FileInfo fileToDownload = new
+                    System.IO.FileInfo("C:\\download\\" + file.FileName);
+                System.Web.HttpContext.Current.Response.Flush();
+                System.Web.HttpContext.Current.Response.WriteFile(fileToDownload.FullName);
+            }
+            catch (System.Exception e)
+            {
+               Console.WriteLine(e.Message);
+            }
+        }
+
+
+        public void Upload(HttpPostedFileBase file)
+        {
+            try
+            {
+                Stream inStream = file.InputStream;
+
+                using (FileStream fs = File.OpenWrite("C:\\upload\\" + file.FileName))
+                {
+                    inStream.CopyTo(fs);
+                    fs.Flush();
+                }
+            }
+            catch (Exception e)
+            {
+                string errMsg = e.Message;
+            }
+        }
+
+
+    }
+}
